d412752a87beb49907fe59207f3f3d01388f3899
Tambeej <bejella@gmail.com>
2025-08-07 09:49:44 +0300

    Step 2 Done

--- quicknotes/src/Form.jsx
+++ quicknotes/src/Form.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function Form() {
   const [noteText, setNoteText] = useState("");
   const [notes, setNotes] = useState([]);
+
   const handleAddNote = () => {
     if (!noteText.trim()) return;
 
@@ -14,6 +15,16 @@ export default function Form() {
     setNotes([newNote, ...notes]);
     setNoteText("");
   };
+
+  const handleDeleteNote = (indexToDelete) => {
+    const shouldDelete = window.confirm(
+      "Are you sure you want to delete your note?"
+    );
+    if (!shouldDelete) return;
+    const filteredNotes = notes.filter((_, index) => index !== indexToDelete);
+    setNotes(filteredNotes);
+  };
+
   return (
     <div className="container">
       <div className="text-container">
@@ -31,8 +42,16 @@ export default function Form() {
       <div className="notes-grid">
         {notes.map((note, index) => (
           <div className="note-card" key={index}>
-            <small style={{ color: "grey" }}>{note.date}</small>
-            <p>{note.text}</p>
+            <div className="date-and-x">
+              <small>{note.date}</small>
+              <small
+                onClick={() => handleDeleteNote(index)}
+                style={{ cursor: "pointer" }}
+              >
+                x
+              </small>
+            </div>
+            <p className="text-note">{note.text}</p>
           </div>
         ))}
       </div>
